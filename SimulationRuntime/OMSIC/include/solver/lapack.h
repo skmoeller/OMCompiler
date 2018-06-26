141829a25ccf737f1e0fe913ee69d48ffec797d3
Andreas <Andreas.Heuermann@fh-bielefeld.de>
2018-06-26 10:46:30 +0200

    WIP OMSI lapack solver - now using new data structure - not tested yet

--- SimulationRuntime/OMSIC/include/solver/lapack.h
+++ SimulationRuntime/OMSIC/include/solver/lapack.h
@@ -34,45 +34,50 @@
 #ifndef _LINEARSOLVERLAPACK_H_
 #define _LINEARSOLVERLAPACK_H_
 
-#include <math.h>
-#include <stdlib.h>
-//#include <string.h> /* memcpy */
-
-#include "math/omsi_matrix.h"
-#include "math/omsi_vector.h"
-#include "math/omsi_math.h"
 #include "omsi.h"
-#include "omsi_eqns_system.h"
-#include "util/rtclock.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*
- * Struct to store all informations for LAPACK dgesv
+ * Struct to store all informations for LAPACK calls
  */
 typedef struct DATA_LAPACK {
     omsi_int        n;      /* number of linear equations*/
     omsi_int        nrhs;   /* number of right hand sides, default =1 */
-    omsi_real*      A;      /* array of dimension (lda,n) */
+    omsi_real*      A;      /* array of dimension (lda,n) containing matrix in
+                             * row major order */
     omsi_int        lda;    /* leading dimension of array A */
-    omsi_int*       ipiv    /* array of dimension n, stores pivot indices for permutation matrix P */
-    omsi_real*      B;      /* array of dimension (ldb, nrhs), right hand side of equation system on entry. On exit if info=0 solution (n x nrhs) Matrix X */
+    omsi_int*       ipiv;   /* array of dimension n, stores pivot indices for
+                             * permutation matrix P */
+    omsi_real*      B;      /* array of dimension (ldb, nrhs), containing right
+                             * hand side of equation system in row major order on entry.
+                             * On exit if info=0 solution (n x nrhs) Matrix X */
     omsi_int        ldb;    /* leading dimension of array B */
     omsi_int        info;   /* =0 if succesfull, <0 if Info=-i the i-th */
+
+    omsi_real*      old_A;  /* contains copy of A for later usage */
+    omsi_real*      old_B;  /* contains copy of B for later usage */
 } DATA_LAPACK;
 
+
+/* extern function prototypes */
+extern omsi_int dgesv_(omsi_int *n, omsi_int *nrhs, omsi_real *a, omsi_int *lda,
+                       omsi_int *ipiv, omsi_real *b, omsi_int *ldb, omsi_int *info);
+extern omsi_int dgemv_(omsi_char* trans, omsi_int* m, omsi_int* n, omsi_real* alpha,
+                       omsi_real* A, omsi_int* lda, omsi_real* x, omsi_int* incx,
+                       omsi_real* beta, omsi_real* y, omsi_int* incy);
+extern omsi_real ddot_(omsi_int* n, omsi_real* dx, omsi_int* incx, omsi_real* dy, omsi_int* incy);
+
 /* function prototypes */
-extern int dgesv_(int *n, int *nrhs, double *a, int *lda, int *ipiv,
-                  double *b, int *ldb, int *info);
-int allocateLapackData(int size, DATA_LAPACK **data);
-int freeLapackData(DATA_LAPACK *data);
-int setLapackData(DATA_LAPACK *lapackData, sim_data_t *sim_data, int n);
-int getLapackData(DATA_LAPACK *lapackData, sim_data_t *sim_data);
-int solveLapack(DATA_LAPACK* lapackData, omsi_t *omsiData,
-                omsi_linear_system_t *linearSystem);
-int solveLapack_new(omsi_t *omsiData, omsi_vector_t *result_x);
+omsi_status solveLapack(omsi_function_t* equationSystemFunc, omsi_callback_functions* callback_functions);
+DATA_LAPACK* set_lapack_data(const omsi_function_t* equationSystemFunc);
+void set_lapack_a (DATA_LAPACK* lapack_data, const omsi_function_t* equationSystemFunc);
+void set_lapack_b (DATA_LAPACK* lapack_data, const omsi_function_t* equationSystemFunc);
+omsi_status eval_residual(DATA_LAPACK* lapack_data);
+void get_result(omsi_function_t* equationSystemFunc, DATA_LAPACK* lapack_data);
+void freeLapackData(DATA_LAPACK* lapack_data);
 
 #ifdef __cplusplus
 }   /* end of extern "C" { */
